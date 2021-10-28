fdf6d662dac48d536f514d253bbcc7972cc331bd
Egor Dmitriev <egordmitriev2@gmail.com>
2021-10-28 21:24:21 +0200

    Added note template building

---
+++ notes/Meta/Template/to_pdf.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+SCRIPT_PATH="$(realpath $(dirname "$0"))"
+INPUT_FILE="$(realpath "$1")"
+WORKDIR="$(dirname $INPUT_FILE)"
+OUTPUT_FILE="${INPUT_FILE/md/pdf}"
+
+PANDOC_PREFIX="$WORKDIR" pandoc -s -o "$OUTPUT_FILE" -t latex -i "$INPUT_FILE" --template="${SCRIPT_PATH}/main.tex" --filter="${SCRIPT_PATH}/uphead" --resource-path="$WORKDIR"
