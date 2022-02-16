5a921dd7dc304b3c45fa85f67436c5cbc85e5b07
Egor Dmitriev <egordmitriev2@gmail.com>
2022-02-16 18:11:11 +0100

    feat: Added star wars dataset into snapshot splitting

--- code/ml/ml/data/datasets.py
+++ code/ml/ml/data/datasets.py
@@ -1,4 +1,5 @@
 from typing import Optional, Callable
+import itertools as it
 
 import numpy as np
 import pandas as pd
@@ -156,36 +157,68 @@ class StarWarsHomogenous(InMemoryDataset):
             for label, entity_schema in schema.edges.items()
         }
 
-        edges_df = pd.concat(edges_dfs.values(), axis=0)
-        src = edges_df[['src', 'dst']].min(axis=1)
-        dst = edges_df[['src', 'dst']].max(axis=1)
-        edges_df = edges_df.assign(src=src, dst=dst)
-        edges_df = edges_df\
-            .drop_duplicates(subset=['src', 'dst'], keep='last')
+        timestamps = set(it.chain(*[
+            edges_dfs[label]['timestamp'].unique()
+            for label, df in edges_dfs.items()
+        ]))
+
+        def deduplicate_edges(edges_dfs):
+            edges_df = pd.concat(edges_dfs.values(), axis=0)
+            src = edges_df[['src', 'dst']].min(axis=1)
+            dst = edges_df[['src', 'dst']].max(axis=1)
+            edges_df = edges_df.assign(src=src, dst=dst)
+            edges_df = edges_df\
+                .drop_duplicates(subset=['src', 'dst'], keep='last')
+            return edges_df
+
+        def create_heterodata(nodes_dfs, edges_dfs):
+            # Create pyg data
+            data = HeteroData()
+            for ntype, ndf in nodes_dfs.items():
+                columns = [c for c in ndf.columns if c.startswith('feat_')]
+                data[ntype].x = torch.tensor(ndf[columns].values.astype(np.float32))
+                if 'timestamp' in ndf.columns:
+                    data[ntype].timestamp = torch.tensor(ndf['timestamp'].values.astype(np.int32))
+
+            for etype, edf in edges_dfs.items():
+                columns = [c for c in edf.columns if c.startswith('feat_')]
+                edge_schema = schema.edges[etype]
+                edge_type = (edge_schema.source_type, edge_schema.get_type(), edge_schema.target_type)
+                data[edge_type].edge_attr = torch.tensor(edf[columns].values.astype(np.float32))
+                data[edge_type].edge_index = torch.tensor(edf[['src', 'dst']].T.values.astype(np.int64))
+                if 'timestamp' in edf.columns:
+                    data[edge_type].timestamp = torch.tensor(edf['timestamp'].values.astype(np.int32))
+
+            used_nodes = set()
+            for estore in data.edge_stores:
+                row, col = estore.edge_index
+                used_nodes.update(row.tolist())
+                used_nodes.update(col.tolist())
+
+            node_mask = torch.zeros(data.num_nodes, dtype=torch.bool).scatter_add(
+                0, torch.tensor(list(used_nodes)), torch.ones(len(used_nodes), dtype=torch.bool)
+            )
+            data.node_stores[0].node_mask = node_mask
+
+            return data
 
         edges_dfs = {
-            'INTERACTIONS': edges_df
+            'INTERACTIONS': deduplicate_edges(edges_dfs),
         }
-
-        # Create pyg data
-        data = HeteroData()
-        for ntype, ndf in nodes_dfs.items():
-            columns = [c for c in ndf.columns if c.startswith('feat_')]
-            data[ntype].x = torch.tensor(ndf[columns].values.astype(np.float32))
-            if 'timestamp' in ndf.columns:
-                data[ntype].timestamp = torch.tensor(ndf['timestamp'].values.astype(np.int32))
-
-        for etype, edf in edges_dfs.items():
-            columns = [c for c in edf.columns if c.startswith('feat_')]
-            edge_schema = schema.edges[etype]
-            edge_type = (edge_schema.source_type, edge_schema.get_type(), edge_schema.target_type)
-            data[edge_type].edge_attr = torch.tensor(edf[columns].values.astype(np.float32))
-            data[edge_type].edge_index = torch.tensor(edf[['src', 'dst']].T.values.astype(np.int64))
-            if 'timestamp' in edf.columns:
-                data[edge_type].timestamp = torch.tensor(edf['timestamp'].values.astype(np.int32))
+        data = create_heterodata(nodes_dfs, edges_dfs)
 
         self.data, self.slices = self.collate([data])
         self.G = DataGraph.from_schema(schema) # TODO: assumes ids are unchanged
+
+        self.snapshots = [
+            create_heterodata(nodes_dfs, {
+                'INTERACTIONS': deduplicate_edges({
+                    k: v[v['timestamp'] == t]
+                    for k, v in edges_dfs.items()
+                })
+            })
+            for t in timestamps
+        ]
 
     def node_mapping(self):
         return list(range(self.data.num_nodes))
