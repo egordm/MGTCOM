d04a64cf20c922a6c48667ca21e855e01d9e3afe
Egor Dmitriev <egordmitriev2@gmail.com>
2022-03-16 10:58:17 +0100

    feat: Added datamodule combining the positive sampling and node batching for prediction

--- code/ml/ml/models/positional.py
+++ code/ml/ml/models/positional.py
@@ -1,12 +1,17 @@
-from typing import Any, List
+from typing import Any, List, Union, Dict
 
 import pytorch_lightning as pl
 import torch.nn
-from pytorch_lightning.utilities.types import STEP_OUTPUT
+from pytorch_lightning.utilities.types import STEP_OUTPUT, EVAL_DATALOADERS, TRAIN_DATALOADERS
+from tch_geometric.loader.hgt_loader import HGTLoader
+from tch_geometric.transforms import NegativeSamplerTransform
+from tch_geometric.transforms.hgt_sampling import HGTSamplerTransform
 from torch import Tensor
 from torch_geometric.data import HeteroData
+from torch_geometric.typing import NodeType
 
 from ml.layers import HingeLoss, NegativeEntropyRegularizer
+from ml.loaders import ContrastiveDataLoader, HeteroEdgesDataset, HeteroNodesDataset
 
 
 class PositionalModel(pl.LightningModule):
@@ -15,6 +20,7 @@ class PositionalModel(pl.LightningModule):
             embedding_module: torch.nn.Module,
             clustering_module: torch.nn.Module,
             ne_weight: float = 0.001,
+            lr: float = 0.01,
             use_clustering: bool = False,
             *args: Any, **kwargs: Any
     ) -> None:
@@ -23,6 +29,7 @@ class PositionalModel(pl.LightningModule):
         self.clustering_module = clustering_module
 
         self.ne_weight = ne_weight
+        self.lr = lr
         self.use_clustering = use_clustering
 
         self.positional_loss = HingeLoss()
@@ -77,11 +84,51 @@ class PositionalModel(pl.LightningModule):
         super().on_predict_epoch_end(results)
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=0.01)
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
         return {
             'optimizer': optimizer,
         }
 
 
+class PositionalDataModule(pl.LightningDataModule):
+    def __init__(
+            self,
+            data: HeteroData,
+            num_samples: Union[List[int], Dict[NodeType, List[int]]],
+            num_neg_samples: int = 3,
+            num_neg_tries: int = 5,
+            batch_size: int = 16,
+            num_workers: int = 0,
+            prefetch_factor=2,
+            persistent_workers=False,
+            temporal: bool = False,
+            **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.data = data
+        self.temporal = temporal
+        self.loader_args = dict(
+            batch_size=batch_size,
+            num_workers=num_workers,
+            prefetch_factor=prefetch_factor,
+            persistent_workers=persistent_workers,
+        )
+        self.neg_sampler = NegativeSamplerTransform(data, num_neg_samples, num_neg_tries)
+        self.neighbor_sampler = HGTSamplerTransform(data, num_samples, temporal=temporal)
 
+    def train_dataloader(self) -> TRAIN_DATALOADERS:
+        return ContrastiveDataLoader(
+            HeteroEdgesDataset(self.data, temporal=self.temporal),
+            self.neg_sampler,
+            neighbor_sampler=self.neighbor_sampler,
+            shuffle=True,
+            **self.loader_args,
+        )
 
+    def predict_dataloader(self) -> EVAL_DATALOADERS:
+        return HGTLoader(
+            HeteroNodesDataset(self.data, temporal=self.temporal),
+            neighbor_sampler=self.neighbor_sampler,
+            shuffle=False,
+            **self.loader_args,
+        )
