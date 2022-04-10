c1bd64613473cfdb487c0d3fced435cdfdfe93be
Egor Dmitriev <egordmitriev2@gmail.com>
2022-04-10 13:26:08 +0200

    refactor: Renamed preclustering monitor

--- source/ml/ml/callbacks/preclustering_monitor.py
+++ source/ml/ml/callbacks/preclustering_monitor.py
@@ -1,6 +1,8 @@
-from typing import Optional
+from pathlib import Path
+from typing import Optional, Dict, Tuple
 
 import torch
+from torch import Tensor
 from pytorch_lightning import Trainer, LightningModule
 from pytorch_lightning.callbacks import Callback
 from pytorch_lightning.trainer.states import RunningStage
@@ -8,9 +10,13 @@ from pytorch_lightning.trainer.states import RunningStage
 from datasets.utils.conversion import igraph_from_hetero
 from ml.layers.clustering import KMeans
 from ml.layers.metrics import newman_girvan_modularity, silhouette_score, davies_bouldin_score
+from ml.models.base.embedding import BaseEmbeddingModel
+from shared import get_logger
+
+logger = get_logger(Path(__file__).stem)
 
 
-class ModularityMonitorCallback(Callback):
+class PreclusteringMonitor(Callback):
     def setup(self, trainer: Trainer, pl_module: LightningModule, stage: Optional[str] = None) -> None:
         super().setup(trainer, pl_module, stage)
         self.data = pl_module.dataset.data
@@ -20,38 +26,36 @@ class ModularityMonitorCallback(Callback):
         self.k = len(comm)
         self.sim = pl_module.hparams.sim
         self.clus = KMeans(pl_module.hparams.repr_dim, self.k, sim=self.sim)
+        self.repr_dim = pl_module.hparams.repr_dim
 
-    def on_validation_epoch_end(self, trainer: Trainer, pl_module: LightningModule) -> None:
-        super().on_validation_epoch_end(trainer, pl_module)
+    def on_validation_epoch_end(self, trainer: Trainer, pl_module: BaseEmbeddingModel) -> None:
         if trainer.state.stage != RunningStage.VALIDATING:
             return
 
-        emb_dict = pl_module.val_emb_dict
-
-        emb_flat = torch.zeros((self.G.vcount(), pl_module.hparams.repr_dim))
-        for node_type, offset in self.node_offsets.items():
-            emb_flat[offset:offset + len(emb_dict[node_type])] = emb_dict[node_type]
-
-        self.clus.fit(emb_flat)
-        I_flat = self.clus.assign(emb_flat)
+        logger.info(f"Running preclustering")
+        emb_dict = pl_module.val_embs
+        emb_flat, I_flat = self.run_preclutering(emb_dict)
         I = {
             node_type: I_flat[offset:offset + len(emb_dict[node_type])]
             for node_type, offset in self.node_offsets.items()
         }
 
         modularity = newman_girvan_modularity(self.data, I, self.k)
-        sc = silhouette_score(emb_flat.numpy(), I_flat.numpy(), sim=self.sim)
-        db = davies_bouldin_score(emb_flat.numpy(), I_flat.numpy(), sim=self.sim)
+        sc = silhouette_score(emb_flat, I_flat, sim=self.sim)
+        db = davies_bouldin_score(emb_flat, I_flat, sim=self.sim)
 
         pl_module.log_dict({
             "epoch_m": modularity,
             "epoch_sc": sc,
             "epoch_dbs": db,
-        }, prog_bar=True)
+        }, prog_bar=True, logger=True)
 
-        for logger in trainer.loggers:
-            logger.log_metrics({
-                "modularity": modularity,
-                "silhouette_score": sc,
-                "davies_bouldin_score": db
-            }, step=trainer.global_step)
+    def run_preclutering(self, emb_dict: Dict[str, Tensor]) -> Tuple[Tensor, Tensor]:
+        emb_flat = torch.zeros((self.G.vcount(), self.repr_dim))
+        for node_type, offset in self.node_offsets.items():
+            emb_flat[offset:offset + len(emb_dict[node_type])] = emb_dict[node_type]
+
+        self.clus.fit(emb_flat)
+        I_flat = self.clus.assign(emb_flat)
+
+        return emb_flat, I_flat
